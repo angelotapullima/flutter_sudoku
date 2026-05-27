d220cb619cf403cf8e6a00203ef452df9f89d57b
Angelo <34034981+angelotapullima@users.noreply.github.com>
2026-05-27 01:35:11 -0500

    update web url

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:numbra/main.dart';
+import 'package:sudoku_arena/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
