d37ce407d8631c975b39c0dd5de6328d68548a7b
Angelo <34034981+angelotapullima@users.noreply.github.com>
2026-05-28 00:07:24 -0500

    fix build

---
+++ android/app/src/main/kotlin/com/anked/dev/sudoku_arena/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.anked.dev.sudoku_arena
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/anked/sudoku_arena/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.anked.sudoku_arena
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
